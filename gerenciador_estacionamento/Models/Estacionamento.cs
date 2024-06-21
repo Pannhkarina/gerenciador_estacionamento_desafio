da0933e4e7e563a3ab4c7fca4e6b641271dd84dd
Pamela Karina Canale <pamela.karina@hotmail.com>
2024-06-20 22:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ gerenciador_estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gerenciador_estacionamento.Models
+{
+    public class Estacionamento
+    {
+        List<string> veiculos = new();
+        int veiculoId = 1;
+        public void AdicionarVeiculos()
+        {
+
+            Console.WriteLine("Informe seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Informe placa do veículo: ");
+            string placa = Console.ReadLine();
+
+            veiculos.Add($"{veiculoId} | {nome} | {placa} ");
+            veiculoId++;
+        }
+
+        public void ListarVeiculos() 
+        {
+            if (veiculos.Any())
+            {
+                foreach (var item in veiculos)
+                {
+                    Console.WriteLine(item + "\n");
+                }
+
+            }
+            else { Console.WriteLine("Nenhum foi veiculo cadastrado."); }
+        
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Informe a placa do carro que deseja remover: ");
+            string placa = Console.ReadLine();
+
+            if (veiculos.Remove(placa))
+            {
+                Console.WriteLine($"Veiculo com placa {placa} removido com sucesso!");
+            }
+            else { Console.WriteLine($"Placa {placa} não encontrada."); }
+           
+       
+        }
+
+
+    }
+}
+ 
---
+++ gerenciador_estacionamento/Program.cs
@@ -0,0 +1,67 @@
+﻿using gerenciador_estacionamento.Models;
+
+decimal precoPorHora;
+
+Estacionamento estacionamento = new();
+
+Console.WriteLine("Olá, seja bem vindo ao estacionamento Relâmpago Mcqueen!\n" +
+                  "Informe o preço inicial: ");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Menu estacionamento 
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (opcao = Console.ReadLine())
+    {
+        case "1":
+            estacionamento.AdicionarVeiculos(); 
+            break;
+        case "2":
+            estacionamento.RemoverVeiculo();
+            break;
+        case "3":
+            estacionamento.ListarVeiculos();
+            break;
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+}
+
+Console.WriteLine("Quantas Horas o veiculo ficou:");
+int quantidadeHoras = Convert.ToInt32(Console.ReadLine()); 
+
+decimal total = quantidadeHoras * precoPorHora;
+
+Console.WriteLine($"Valor total: {total}");
+
+Console.WriteLine("O programa se encerrou");
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
